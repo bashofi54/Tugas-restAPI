a95b23dda75aac30473911c316474c2541016a37
bashofi54 <bashofi54@gmail.com>
2022-08-19 17:03:05 +0700

    gud

--- app/product_v2/routes.js
+++ app/product_v2/routes.js
@@ -8,6 +8,6 @@ router.get('/product', prodContrl.index2);
 router.get('/product/:id', prodContrl.view2);
 router.post('/product',upload.single('image'), prodContrl.store2);
 router.put('/product/:id', prodContrl.update2);
-router.delete('/product/id', prodContrl.destroy2);
+router.delete('/product/:id', prodContrl.destroy2);
 
 module.exports = router;
